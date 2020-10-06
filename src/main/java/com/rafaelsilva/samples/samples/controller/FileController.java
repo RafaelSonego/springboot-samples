c7c85e7c5db2e3a1b5b766c8694178ecad13680e
rafaelsonegosilva <silvarafael.ca@gmail.com>
2020-10-06 18:30:04 -0400

    Adding log4j2

--- src/main/java/com/rafaelsilva/samples/samples/controller/FileController.java
+++ src/main/java/com/rafaelsilva/samples/samples/controller/FileController.java
@@ -1,5 +1,6 @@
 package com.rafaelsilva.samples.samples.controller;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,13 @@ import com.rafaelsilva.samples.samples.data.repository.FileGeneratedRepository;
 
 @Controller
 public class FileController {
-
+	private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(FileController.class);
 	@Autowired
 	FileGeneratedRepository fileRepo;
 	
 	@RequestMapping("/")
 	public String home() {
+		LOGGER.info("We are here!!!!");
 		return "sampleFiles/GenerateFiles";
 	}
 	
